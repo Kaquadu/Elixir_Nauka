7eb870aaa1a95c259e772fba43bcc5ac98f286a8
Toolmaster <kuba.kowalczykowski@student.put.poznan.pl>
2018-09-27 13:13:08 +0200

    Calculator
    Calculator with imaginary numbers

--- Basics/calculator.exs
+++ Basics/calculator.exs
@@ -1,24 +1,36 @@
 defmodule Calculator do
+  #sum
   def sum(a,b) do
-    a + b
+    (a + b)
   end
+  #substraction
   def sub(a,b) do
-    a - b
+    (a - b)
   end
+  #multiplication
   def mul(a, b) do
-    a * b
+    (a * b)
   end
+  #div
   def div(a, 0), do: "incorrect"
   def div(a, b) do
-    a / b
+    (a / b)
   end
-  def factorial(0), do: 1
+  #factorial
+  def factorial(0), do:  (1)
   def factorial(n) do
     n * (n - 1)
   end
-  def sqrt(n) do
-    :math.sqrt(n)
+  #square root
+  def sqrt(n) when n >= 0 do
+      :math.sqrt(n)
   end
+  def sqrt(n) when n < 0 do
+      Enum.join([to_string(:math.sqrt(abs(n))), " + i, ", to_string(:math.sqrt(abs(n))), " - i"])
+  end
+
 end
 
-IO.puts (Calculator.sqrt(6))
+IO.puts Calculator.sqrt(-5)
+IO.puts Calculator.div(5, 0)
+IO.puts Calculator.sub(6, 2)
